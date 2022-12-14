04a04feaa3f83879e07acfe43c435963017d33b9
Alexey <117473731+AlexeyKost@users.noreply.github.com>
2022-12-14 22:40:06 +0300

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    System.out.println(service.calculate(amount, registered));
   }
 }
 
